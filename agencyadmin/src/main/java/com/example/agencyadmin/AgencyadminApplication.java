e3773acb4c12e28d7cc168939574573a2af72a26
Ngoh1919 <aghet87@gmail.com>
2025-11-01 13:26:13 +0100

    added spring boot and some dependencies

---
+++ agencyadmin/src/main/java/com/example/agencyadmin/AgencyadminApplication.java
@@ -0,0 +1,13 @@
+package com.example.agencyadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyadminApplication.class, args);
+	}
+
+}
---
+++ agencyadmin/src/test/java/com/example/agencyadmin/AgencyadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agencyadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
